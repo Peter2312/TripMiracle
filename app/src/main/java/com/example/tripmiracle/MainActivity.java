b40969241280e3ee78b900f53888336c6fff8154
BrianFrost005 <brianlzx.bl@gmail.com>
2022-11-22 20:57:21 +0800

    forgot password to signup

--- app/src/main/java/com/example/tripmiracle/MainActivity.java
+++ app/src/main/java/com/example/tripmiracle/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.tripmiracle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //declare elements
+    //inflate activity_main layout to access elements easier
+    LayoutInflater l = getLayoutInflater();
+    View v = l.inflate(R.layout.activity_main, null);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
