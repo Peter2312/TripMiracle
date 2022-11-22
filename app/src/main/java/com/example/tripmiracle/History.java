ad94b0e316e7f4a1c34acb8a007fb99dd1c0bf22
github_Peter2312 <pangkheryong@gmail.com>
2022-11-22 22:37:06 +0800

    Created

--- app/src/main/java/com/example/tripmiracle/History.java
+++ app/src/main/java/com/example/tripmiracle/History.java
@@ -2,13 +2,33 @@ package com.example.tripmiracle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.tripmiracle.databinding.ActivityHomePageBinding;
 
 public class History extends AppCompatActivity {
+
+    private ActivityHomePageBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        binding = ActivityHomePageBinding.inflate(getLayoutInflater());
+        LinearLayout view = binding.getRoot();
+        setContentView(view);
+
+        binding.bookHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(History.this, HomePage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tripmiracle/HomePage.java
+++ app/src/main/java/com/example/tripmiracle/HomePage.java
@@ -2,13 +2,51 @@ package com.example.tripmiracle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.tripmiracle.databinding.ActivityHomePageBinding;
 
 public class HomePage extends AppCompatActivity {
+
+    private ActivityHomePageBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        binding = ActivityHomePageBinding.inflate(getLayoutInflater());
+        LinearLayout view = binding.getRoot();
+        setContentView(view);
+
+        binding.booking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, BookActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        binding.checkBooking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, CheckBooking.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        binding.bookHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, History.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
