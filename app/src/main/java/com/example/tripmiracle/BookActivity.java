39afa10a83bfb6f7bc8d0fc85e143319e34367be
github_Peter2312 <pangkheryong@gmail.com>
2022-11-23 02:04:58 +0800

    Created

--- app/src/main/java/com/example/tripmiracle/BookActivity.java
+++ app/src/main/java/com/example/tripmiracle/BookActivity.java
@@ -2,27 +2,56 @@ package com.example.tripmiracle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.LinearLayout;
 
+import com.example.tripmiracle.databinding.ActivityBookBinding;
 import com.example.tripmiracle.databinding.ActivityHomePageBinding;
+
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class BookActivity extends AppCompatActivity {
 
-    private ActivityHomePageBinding binding;
+    private ActivityBookBinding binding;
+    DatePickerDialog pickerDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
 
-        binding = ActivityHomePageBinding.inflate(getLayoutInflater());
+        binding = ActivityBookBinding.inflate(getLayoutInflater());
         LinearLayout view = binding.getRoot();
         setContentView(view);
 
-        binding.booking.setOnClickListener(new View.OnClickListener() {
+        binding.selectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar calendar = Calendar.getInstance();
+                calendar.setTimeZone(TimeZone.getTimeZone("Asia/Kuala_Lumpur"));
+                int chosenDay = calendar.get(Calendar.DAY_OF_MONTH);
+                int chosenMonth = calendar.get(Calendar.MONTH);
+                int chosenYear = calendar.get(Calendar.YEAR);
+
+                //DatePicker dialog
+                pickerDate = new DatePickerDialog(BookActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                        binding.displayDate.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
+                    }
+                }, chosenYear, chosenMonth, chosenDay);
+
+                pickerDate.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+                pickerDate.show();
+            }
+        });
+
+        binding.returnHomePage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(BookActivity.this, HomePage.class);
@@ -30,5 +59,13 @@ public class BookActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        binding.book.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
