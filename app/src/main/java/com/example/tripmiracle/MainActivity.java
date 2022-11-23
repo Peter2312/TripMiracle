5811f6cb0e2f9969943a79564cdca378cb1452f1
BrianFrost005 <brianlzx.bl@gmail.com>
2022-11-24 01:42:36 +0800

    app executable with login

--- app/src/main/java/com/example/tripmiracle/MainActivity.java
+++ app/src/main/java/com/example/tripmiracle/MainActivity.java
@@ -1,31 +1,133 @@
 package com.example.tripmiracle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.tripmiracle.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button login;
+    public static final String TAG = "MainActivity";
+
+    private ActivityMainBinding binding;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private DocumentReference adminRef = db.collection("admin").document();
+    private DocumentReference userRef = db.collection("user").document();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        login = findViewById(R.id.loginbtn);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        LinearLayout view = binding.getRoot();
+        setContentView(view);
 
-        login.setOnClickListener(new View.OnClickListener() {
+        mAuth =FirebaseAuth.getInstance();
+
+        binding.loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, HomePage.class);
-                startActivity(intent);
-                finish();
+                signIn();
             }
         });
+
+        binding.registerbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registration();
+            }
+        });
+    }
+
+    private void signIn() {
+        mAuth.signInWithEmailAndPassword(binding.emailEt.getText().toString(), binding.passwordEt.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(MainActivity.this, HomePage.class);
+                            intent.putExtra(HomePage.USERNAME, binding.usernameEt.getText().toString());
+                            startActivity(intent);
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void registration() {
+        String userId = userRef.getId();
+        User user = new User(binding.usernameEt.getText().toString(), userId);
+
+
+        mAuth.createUserWithEmailAndPassword(binding.emailEt.getText().toString(), binding.passwordEt.getText().toString()).addOnCompleteListener( this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d( TAG, "createUserWithEmail:success" );
+                    Toast.makeText(MainActivity.this, "Authentication successful", Toast.LENGTH_SHORT).show();
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI( user );
+                } else {
+                    Log.w(TAG, "createUserWithEmail:failed");
+                    Toast.makeText(MainActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                    updateUI( null );
+                }
+            }
+        } );
+
+        userRef.set(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            reload();
+        }
+    }
+
+    private void reload() {
+
+    }
+
+    private void updateUI(FirebaseUser user) {
 
     }
 }
